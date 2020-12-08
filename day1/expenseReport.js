903533115bc889c3e4316a04d0573524c0ec97bf
India Rex <face940@gmail.com>
2020-12-08 10:41:37 +0000

    Add basic readfile functionality to expensereport

--- day1/expenseReport.js
+++ day1/expenseReport.js
@@ -1,2 +1,12 @@
-let message = "Hello, world";
-console.log(message);
+const fs = require('fs');
+
+fs.readFile('input.txt', 'utf8', function(err, data) {
+    if(err) {
+        console.log(err.message);
+    } else {
+        console.log(data);
+    }
+});
+
+// let message = "Hello, world";
+// console.log(message);
